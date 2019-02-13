f7d2d45534eacf65c5247ef0f54ed0493ecc51e6
ambarish <ambarish12@gmail.com>
2019-02-14 00:39:43 +0530

    dao added

---
+++ dao/mongoQueries.js
@@ -0,0 +1,14 @@
+const getTxns = function(){
+    return new Promise(function(resolve,reject){
+        resolve(100);
+    });
+};
+const addTxns = function(){
+    return new Promise(function(resolve,reject){
+        resolve(100);
+    });
+};
+module.exports = {
+    getTxns,
+    addTxns
+}
